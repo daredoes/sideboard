c72d3234f3ecb8f055ee407b9bc392b47f0618a8
Victoria Earl <kitsuta@gmail.com>
2016-05-25 02:44:45 +0200

    DO NOT USE, testing only - trying to track down some prioirty wackiness

--- sideboard/internal/imports.py
+++ sideboard/internal/imports.py
@@ -10,8 +10,17 @@ plugins = {}
 
 def _discover_plugins():
     ordered = list(reversed(config['priority_plugins']))
+    print("priority_plugins: " + str(ordered))
     plugin_dirs = [d for d in glob(join(config['plugins_dir'], '*')) if isdir(d) and not basename(d).startswith('_')]
-    for plugin_path in sorted(plugin_dirs, reverse=True, key=lambda d: (ordered.index(d) if d in ordered else 0)):
+
+    print("plugin_dirs: " + str(plugin_dirs))
+    sorted_plugin_ordering = sorted(plugin_dirs, reverse=True, key=lambda d: (ordered.index(d) if d in ordered else 0))
+
+    sorted_plugin_names = [basename(p) for p in sorted_plugin_ordering]
+    print("would load plugins in the following order:" + str(sorted_plugin_names))
+
+    for plugin_path in sorted_plugin_ordering:
         sys.path.append(plugin_path)
         plugin_name = basename(plugin_path).replace('-', '_')
+        print("LOADING PLUGIN: " + plugin_name)
         plugins[plugin_name] = importlib.import_module(plugin_name)
